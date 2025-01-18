34bc7c50185362e74a1958a104a56fa9d8e3d619
belokur.bv@gmail.com <bohdan.belokur@dxc.com>
2025-01-18 14:04:13 +0200

    Initial commit

--- src/main/java/com/belokur/jldbase/FileUtil.java
+++
@@ -1,7 +0,0 @@
-package com.belokur.jldbase;
-
-public class FileUtil {
-//    public String write(String filename, String content) {
-//
-//    }
-}
--- src/main/java/com/belokur/jldbase/api/KeyValueStorage.java
+++ src/main/java/com/belokur/jldbase/api/KeyValueStorage.java
@@ -1,7 +1,12 @@
 package com.belokur.jldbase.api;
 
+import java.util.List;
+import java.util.Map;
+
 public interface KeyValueStorage {
     void set(String key, String value);
 
     String get(String key);
+
+    Map<String, String> getAll();
 }
